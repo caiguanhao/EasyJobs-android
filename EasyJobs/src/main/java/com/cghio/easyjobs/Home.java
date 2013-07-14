e2bde613e642dd628f719d9fdebcdf653a34b0e7
caiguanhao <caiguanhao@gmail.com>
2013-07-14 21:25:57 +0800

    add connect barcode scanner;

--- EasyJobs/src/main/java/com/cghio/easyjobs/Home.java
+++ EasyJobs/src/main/java/com/cghio/easyjobs/Home.java
@@ -1,8 +1,14 @@
 package com.cghio.easyjobs;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class Home extends Activity {
 
@@ -10,6 +16,38 @@ public class Home extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        findViewById(R.id.button_scan_qr_code).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                    intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
+                    startActivityForResult(intent, 1);
+                } catch (ActivityNotFoundException e) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(Home.this).create();
+                    alertDialog.setTitle(R.string.error);
+                    alertDialog.setMessage(getString(R.string.error_barcode_scanner_not_installed));
+                    alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.cancel),
+                            new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                        }
+                    });
+                    alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok),
+                            new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                            intent.setData(Uri.parse(
+                                    "market://details?id=com.google.zxing.client.android"));
+                            startActivity(intent);
+                        }
+                    });
+                    alertDialog.show();
+                }
+            }
+        });
     }
 
 
@@ -19,5 +57,14 @@ public class Home extends Activity {
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
-    
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            String content = intent.getStringExtra("SCAN_RESULT");
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setMessage(content);
+            alertDialog.setTitle("content");
+            alertDialog.show();
+        }
+    }
 }
