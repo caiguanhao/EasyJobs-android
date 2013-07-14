60028a69a189201035f48f0f7aee3cebf57c7ff7
caiguanhao <caiguanhao@gmail.com>
2013-07-14 23:10:59 +0800

    save data to prefs and read prefs on start;

--- EasyJobs/src/main/java/com/cghio/easyjobs/Home.java
+++ EasyJobs/src/main/java/com/cghio/easyjobs/Home.java
@@ -5,12 +5,14 @@ import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Message;
 import android.util.Base64;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -20,6 +22,15 @@ import java.net.URL;
 
 public class Home extends Activity {
 
+    private static String PREF_FILE = "auth_info";
+    private static String PREF_VERSION = "VERSION";
+    private static String PREF_URL = "URL";
+    private static String PREF_CONTENT = "CONTENT";
+
+    public static int API_VERSION = 0;
+    public static URL API_HELP_URL = null;
+    public static String API_TOKEN = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,6 +39,10 @@ public class Home extends Activity {
         findViewById(R.id.button_scan_qr_code).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (readPrefs()) {
+                    showSimpleErrorDialog(getString(R.string.error_already_authenticated));
+                    return;
+                }
                 try {
                     Intent intent = new Intent("com.google.zxing.client.android.SCAN");
                     intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
@@ -96,6 +111,14 @@ public class Home extends Activity {
 
                     if (CONTENT.length() == 0)
                         throw new Exception(getString(R.string.error_invalid_content));
+
+                    SharedPreferences sharedPrefs = getSharedPreferences(PREF_FILE, 0);
+                    SharedPreferences.Editor editor = sharedPrefs.edit();
+                    editor.putInt(PREF_VERSION, VERSION);
+                    editor.putString(PREF_URL, URL);
+                    editor.putString(PREF_CONTENT, CONTENT);
+                    editor.commit();
+
                 } catch (MalformedURLException e) {
                     showSimpleErrorDialog(getString(R.string.error_invalid_url));
                 } catch (JSONException e) {
@@ -103,6 +126,7 @@ public class Home extends Activity {
                 } catch (Exception e) {
                     showSimpleErrorDialog(e.getMessage());
                 }
+                Toast.makeText(Home.this, R.string.successfully_scanned, Toast.LENGTH_SHORT).show();
             }
         }
     }
@@ -113,5 +137,32 @@ public class Home extends Activity {
         alertDialog.setTitle(R.string.error);
         alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.ok), (Message) null);
         alertDialog.show();
+    }
+
+    private boolean readPrefs() {
+        SharedPreferences sharedPrefs = getSharedPreferences(PREF_FILE, 0);
+        int VERSION = sharedPrefs.getInt(PREF_VERSION, 0);
+        String URL = sharedPrefs.getString(PREF_URL, "");
+        String CONTENT = sharedPrefs.getString(PREF_CONTENT, "");
+
+        // validate version number
+        if (VERSION <= 0) return false;
+        if (VERSION > getResources().getInteger(R.integer.max_api_version)) return false;
+
+        // validate help URL
+        URL url;
+        try {
+            url = new URL(URL);
+        } catch (MalformedURLException e) {
+            return false;
+        }
+
+        if (CONTENT.length() == 0) return false;
+
+        API_VERSION = VERSION;
+        API_HELP_URL = url;
+        API_TOKEN = CONTENT;
+
+        return true;
     }
 }
